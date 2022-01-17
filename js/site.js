27b37bdae9a0a1a67723469db3844449184a42c2
Dillon <55114897+DillxnR@users.noreply.github.com>
2022-01-17 00:59:27 +0000

    Delete site.js

--- js/site.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('a').onmousemove = (e) => {
-  const long = e.pageX - e.target.offsetLeft
-  const lat = e.pageY - e.target.offsetTop
-  e.target.style.setProperty('--long', `${ long }px`)
-  e.target.style.setProperty('--lat', `${ lat }px`)
-}
